42a0fed2a83d70c5b3b2c91f2558488c567dce34
Tasreeq Adams <81373672+Tasreeq-Adams@users.noreply.github.com>
2022-06-14 12:34:29 +0200

    Domain entity City and Country Code
    Domain entity City and Country Code

--- src/main/java/za/ac/cput/domain/City.java
+++ src/main/java/za/ac/cput/domain/City.java
@@ -5,5 +5,85 @@ Date:june 2022
  */
 package za.ac.cput.domain;
 
+import java.util.Objects;
+
 public class City {
+
+    private String id, name;
+    private Country country;
+
+
+    private City(Builder builder){
+        this.id = builder.id;
+        this.name = builder.name;
+        this.country = builder.country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id.equals(city.id) && name.equals(city.name) && country.equals(city.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, country);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", country=" + country +
+                '}';
+    }
+
+
+    public static class Builder{
+        private String id, name;
+        private Country country;
+
+        public Builder Id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder Name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder Country(Country country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder copy(City city)
+        {
+            this.id = city.id;
+            this.name = city.name;
+            this.country = city.country;
+            return this;
+        }
+        public City build(){
+            return new City(this);
+        }
+    }
+
 }
--- src/main/java/za/ac/cput/domain/Country.java
+++ src/main/java/za/ac/cput/domain/Country.java
@@ -5,5 +5,69 @@ Date:june 2022
  */
 package za.ac.cput.domain;
 
+import java.util.Objects;
+
 public class Country {
+    private String id;
+    private String name;
+
+    private Country(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return id.equals(country.id) && name.equals(country.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private String id, name;
+
+        public Builder Id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder Name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder copy (Country country){
+            this.id = country.id;
+            this.name = country.name;
+            return this;
+        }
+
+        public Country build(){
+            return new Country(this);
+        }
+    }
+
+
 }
